767df5316bc118ea42f7c58a2f62d50b1a42256d
Alghifari Ramadhan <110340334+Alghifari22@users.noreply.github.com>
2024-07-09 22:04:59 +0700

    Add files via upload

---
+++ app/Models/Siswa.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Siswa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'siswa';
+    
+    protected $filltable = [
+        'nis',
+        'name',
+        'tgl_lahir',
+        'jk',
+        'alamat',
+        'jurusan',
+        'kelas',
+        'ws1',
+        'tws1',
+        'ts',
+        'foto',
+        'username',
+    ];
+}
