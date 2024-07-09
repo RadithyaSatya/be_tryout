e3abb816220fcd3d305d12c29cd13b58954f3b7f
Alghifari Ramadhan <110340334+Alghifari22@users.noreply.github.com>
2024-07-09 12:58:56 +0700

    Tambah table guru
    Tambah table guru

---
+++ database/migrations/2024_07_09_054229_guru.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('guru', function (Blueprint $table) {
+            $table->string('id_guru',15)->primary();
+            $table->string('nama',35);
+            $table->date('tgl_lahir');
+            $table->string('jk',1);
+            $table->string('alamat',255);
+            $table->string('no_telp',15);
+            $table->string('foto',40);
+            $table->string('matpel',20);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('guru');
+    }
+};
