846055eb6413802f477bad84503533de923e1d93
Radith <radithyasatya7@gmail.com>
2024-07-15 10:08:00 +0700

    menambahkan fitur login pada authcontroller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        try{
+            $user = User::where("username",$request->username)->first();
+            if(!$user){
+                return response()->json([
+                    "status"=>false,
+                    "message"=>"Username or Password is Incorrect, Please Try Again"
+                ],404);
+            }
+
+            if(Hash::check($request->password, $user->password)){
+                $token = $user->createToken("auth_sanctum")->plainTextToken;
+                return response()->json([
+                    "status"=>true,
+                    "message"=>"Login Succesfully",
+                    "token"=>$token
+                ]);
+            }
+        }
+        catch(\Throwable $th){
+            return response()->json([
+                "status"=>false,
+                "message"=>$th->getMessage()
+            ],500);
+        }
+    }
+}
